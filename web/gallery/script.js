da7e28e5d7241084911fb1777b5a34b4ade8f703
NIVEDCHANDRAN <chandrannived7@gmail.com>
2025-10-07 08:58:14 +0530

    sx

--- web/gallery/script.js
+++ web/gallery/script.js
@@ -4,19 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add your image URLs here. You can add as many as you want.
     const imageUrls = [
-        './1.JPG',
-        './2.JPG',
-        './3.JPG',
-        './4.JPG',
-        './5.JPG',
-        './6.JPG',
-        './7.JPG',
-        './8.JPG',
-        './9.JPG',
-        './10.JPG',
-        './11.JPG',
-        './12.JPG',
-        './13.JPG',
+        './1.jpg',
+        './2.jpg',
+        './3.jpg',
+        './4.jpg',
+        './5.jpg',
+        './6.jpg',
+        './7.jpg',
+        './8.jpg',
+        './9.jpg',
+        './10.jpg',
+        './11.jpg',
+        './12.jpg',
+        './13.jpg',
 
         // Add more image URLs as needed
     ];
